5333f1600e5e60931facead2db49bf09befb4c98
Subhash-0206 <85171207+Subhash-0206@users.noreply.github.com>
2026-05-14 12:34:19 -0600

    Implement selection sort in decreasing order
    Added a method to sort an array in decreasing order using selection sort.

--- src/coding_practice/SelectionSort.java
+++ src/coding_practice/SelectionSort.java
@@ -1,4 +1,33 @@
 class SelectionSort {
+    //sort in descending order
+    public static void sortDecreasingOrder()
+    {
+         //Selection sort
+        int ar[] = {9,1,2,5,4,7,-1};
+        System.out.println("Selection sort of the array:");
+        int n = ar.length;
+        for(int round=0; round < n-1; round++)
+        {
+            int minIdx = round;
+            for(int i=round+1;i<n;i++)
+            {
+                if(ar[i]>ar[minIdx])
+                {
+                    minIdx =i;
+                }
+            }
+            //swap round, minIdx
+            int temp = ar[round];
+            ar[round] = ar[minIdx];
+            ar[minIdx] = temp;
+
+        }
+        System.out.println("Array after Selection sort:");
+        for(int i:ar)
+        {
+            System.out.print(i+" ");
+        }
+    }
     public static void main(String[] args) {
         
          //Selection sort
